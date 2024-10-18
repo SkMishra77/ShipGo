20b375dc4469f2588d18c1a45f4892030c3c866c
SkMishra <sanatkumarmishra02@gmail.com>
2024-10-19 02:22:38 +0530

    commit

--- shipgo-frontend/src/app/page.tsx
+++ shipgo-frontend/src/app/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+import React from "react";
+import {useRouter} from "next/navigation";
+
+
 export default function Home() {
+    const router = useRouter();
     return (
-        <div className={`flex h-screen w-full justify-center items-center font-semibold text-4xl`}>
+        <div className={`flex flex-col h-screen w-full justify-center items-center font-semibold text-4xl pt-10`}>
             Welcome to Atlan assignment!
+            <div className="flex flex-col justify-center items-center w-full h-full space-y-4">
+                <button onClick={() => router.push('/driver')}
+                    className="bg-blue-500 text-white font-bold py-2 px-4 rounded-full shadow-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 transition-all duration-300 ease-in-out">
+                    Driver
+                </button>
+                <button onClick={() => router.push('/user/add/')}
+                    className="bg-blue-500 text-white font-bold py-2 px-4 rounded-full shadow-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 transition-all duration-300 ease-in-out">
+                    User
+                </button>
+            </div>
         </div>
     );
 }
